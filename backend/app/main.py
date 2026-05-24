d458440123661e4d452509ca35c2fe17a4cb6eb0
MichaelAudric <michaelaudric29@gmail.com>
2026-05-24 11:32:42 +0800

    cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -9,7 +9,7 @@ app = FastAPI(title="Skin Disease AI Backend")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:3000",
+        "https://skin-ai-phi.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
